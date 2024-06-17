44e9ba6fe30010303b2d780c15cbb82530106c68
Patel Divyesh <pateldivyesh1323@gmail.com>
2024-06-17 14:30:15 +0530

    Fixed CORS error

--- server/cmd/app/main.go
+++ server/cmd/app/main.go
@@ -6,10 +6,18 @@ import (
 	"net/http"
 
 	"github.com/pateldivyesh1323/futflare/server/internal/router"
+	"github.com/rs/cors"
 )
 
 func main() {
 	fmt.Println("Server fired on http://localhost:8000...")
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // Allow all origins
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
 	r := router.NewRouter()
-	log.Fatal(http.ListenAndServe("127.0.0.1:8000", r))
+	handler := c.Handler(r)
+	log.Fatal(http.ListenAndServe("127.0.0.1:8000", handler))
 }
